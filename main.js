82a94b8b3527241efbc78ac4c8cdfbe72aecb564
cedric.claus <cedric.claus@cite-consulting.be>
2014-05-26 19:31:10 +0200

    ng-repeat

--- main.js
+++ main.js
@@ -4,14 +4,14 @@
 var myApp = angular.module('myApp', []);
 
 myApp.factory("Data",function(){
-    return {message : "i'm data from a service",
-            message2 : " Cedric"
+    return {message : "i'm data from a service"
+
         }
 })
 
 myApp.filter('reverse',function(Data){
-    return function(toto){
-        return toto.split("").reverse().join("") +Data.message2;
+    return function(text){
+        return text.split("").reverse().join("") ;
     }
 })
 
@@ -25,5 +25,76 @@ function SecondCtrl($scope,Data){
     $scope.reversedMessage = function(message) {
         return message.split("").reverse().join("");
     }
+}
+
+myApp.factory('Avengers', function() {
+    var Avengers = {};
+    Avengers.cast = [
+        {
+            name: "Robert Downey Jr.",
+            character: "Tony Stark / Iron Man"
+        },
+        {
+            name: "Chris Evans",
+            character: "Steve Rogers / Captain America"
+        },
+        {
+            name: "Mark Ruffalo",
+            character: "Bruce Banner / The Hulk"
+        },
+        {
+            name: "Chris Hemsworth",
+            character: "Thor"
+        },
+        {
+            name: "Scarlett Johansson",
+            character: "Natasha Romanoff / Black Widow"
+        },
+        {
+            name: "Jeremy Renner",
+            character: "Clint Barton / Hawkeye"
+        },
+        {
+            name: "Tom Hiddleston",
+            character: "Loki"
+        },
+        {
+            name: "Clark Gregg",
+            character: "Agent Phil Coulson"
+        },
+        {
+            name: "Cobie Smulders",
+            character: "Agent Maria Hill"
+        },
+        {
+            name: "Stellan Skarsgard",
+            character: "Selvig"
+        },
+        {
+            name: "Samuel L. Jackson",
+            character: "Nick Fury"
+        },
+        {
+            name: "Gwyneth Paltrow",
+            character: "Pepper Potts"
+        },
+        {
+            name: "Paul Bettany",
+            character: "Jarvis (voice)"
+        },
+        {
+            name: "Alexis Denisof",
+            character: "The Other"
+        },
+        {
+            name: "Tina Benko",
+            character: "NASA Scientist"
+        }
+    ];
+    return Avengers;
+})
+
+function AvengersCtrl($scope, Avengers) {
+    $scope.avengers = Avengers;
 }
 
